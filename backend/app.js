e98a6ccdb8a4d62564d67277da1444490d890353
Wealthometer <wealthhajoh87@gmail.com>
2025-07-26 15:16:20 +0100

    Update

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,14 @@ import express from express;
 import cors from cors;
 
 const app = express();
+dotenv.config({ path : "./config/config.env" });
+
+app.use (
+    cors({
+        origin : [process.env.FRONTEND_URL],
+        methods : ["POST"],
+        credentials : true
+    })
+)
 
 export default app;
